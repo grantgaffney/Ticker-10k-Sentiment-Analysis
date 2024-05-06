4bbfef4ba65429c6a858109b1f6be43804336207
Henry Gaffney <143543517+grantgaffney@users.noreply.github.com>
2024-05-05 22:31:39 -0400

    Comment app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#Author Henry Grant Gaffney 5/5/2024
+
 from bs4 import BeautifulSoup
 import re
 from openai import OpenAI
@@ -12,70 +14,72 @@ from scipy.stats import pearsonr
 from flask_cors import CORS
 import shutil
 
+"""
+Downloads all of the 10-k forms for a ticker from 1995-2023
+"""
 def download(ticker):
     dl = Downloader("Company", "email@domain.com")
 
     dl.get("10-K", ticker, after="1995-01-01", before="2023-12-31", download_details=True)
 
 
+
 client = OpenAI(api_key='API_KEY') #Put your key here
 
 
+
+"""
+Stips all of the html elements from an html file
+"""
 def extract_text_from_html(file_path):
-    """
-    Extracts and cleans text from an HTML file, removing all HTML tags, styles, scripts,
-    inline attributes, and encoded sections that are likely not human-readable text.
-    """
     with open(file_path, 'r', encoding='utf-8') as file:
         soup = BeautifulSoup(file, 'lxml')
 
-        # Remove script, style, meta, and noscript elements
         for element in soup(["script", "style", "meta", "noscript"]):
             element.decompose()
 
-        # Extract text and remove any remaining HTML tags
+        # Remove any remaining HTML tags
         text = soup.get_text(separator=' ', strip=True)
 
-        # Regex to remove Excel-like data
+        # Remove Excel data
         text = re.sub(r'EXCEL.*?END', '', text, flags=re.DOTALL)
 
-        # Remove any residual inline styles or attributes (common in malformed HTML or from copy-pasting from word processors)
-        text = re.sub(r'\{[\s\S]*?\}', '',
-                      text)  # Removes anything that appears within curly braces (common in inline styles)
-        text = re.sub(r'\[[^\]]*\]', '', text)  # Removes anything that appears within square brackets
-        text = re.sub(r'\([^)]*\)', '', text)  # Removes anything in parentheses (often used in documents)
-        text = re.sub(r'\<[^>]*\>', '', text)  # Extra check to strip anything that looks like a tag
-        text = re.sub(r'\s+', ' ', text).strip()  # Normalize whitespace
+        # Remove  inline styles
+        text = re.sub(r'\{[\s\S]*?\}', '', text)
+        text = re.sub(r'\[[^\]]*\]', '', text)
+        text = re.sub(r'\([^)]*\)', '', text)
+        text = re.sub(r'\<[^>]*\>', '', text)
+        text = re.sub(r'\s+', ' ', text).strip()
 
-        # Regex to remove encoded sections, adjust as necessary based on actual data observation
         text = re.sub(r'begin \d+ .+?\n[\s\S]+?\nend', '', text, flags=re.MULTILINE)
 
     return text
 
-
+"""
+Cleans and normalizes text for passing to an LLM
+"""
 def clean_text(text):
-    """
-    Cleans the extracted text by removing special characters and multiple spaces.
-    """
     text = re.sub(r'[^a-zA-Z0-9\s]', ' ', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
-
+"""
+Extracts the MD&A section from a chunk of cleaned text
+"""
 def extract_mdna(text):
-    # Define regex patterns for the start and end of the MD&A section
+    # regex pattern for start and end of MD&A section
     start_pattern = re.compile(
         r"Management s Discussion and Analysis of Financial Condition and Results of Operations", re.IGNORECASE)
     end_pattern = re.compile(r"Quantitative and Qualitative Disclosures About Market Risk", re.IGNORECASE)
 
-    # Find all occurrences of the start and end patterns
+    # Find all occurrences
     start_matches = list(start_pattern.finditer(text))
     end_matches = list(end_pattern.finditer(text))
 
     if start_matches and end_matches:
-        # Get the last start match
+        # Get the last
         last_start_match = start_matches[-1]
-        # Find the first end match that occurs after the last start match
+        # Find first end match that occurs after  last start match
         for end_match in end_matches:
             if end_match.start() > last_start_match.end():
                 mdna_text = text[last_start_match.end():end_match.start()]
@@ -83,38 +87,32 @@ def extract_mdna(text):
 
     return "MD&A section not found or markers do not match."
 
-
+"""
+Wrapper to completely clean and normalize text from an html
+"""
 def process_file(file):
     html_text = extract_text_from_html(file)
     clean_html_text = clean_text(html_text)
 
     return clean_html_text
 
-
+"""
+Helper method to get the fiscal year from text
+"""
 def extract_fiscal_year(cleaned_text):
-    """
-    Extracts the fiscal year from cleaned text using regex patterns.
-
-    Args:
-        cleaned_text (str): The cleaned text from which to extract the fiscal year.
-
-    Returns:
-        str: The extracted fiscal year, or an error message if not found.
-    """
-    # Use regex to find the fiscal year (case-insensitive)
     fiscal_year_pattern = re.compile(r"For the fiscal year ended ([a-zA-Z]+ \d{1,2} \d{4})", re.IGNORECASE)
     search_result = fiscal_year_pattern.search(cleaned_text)
 
     if search_result:
-        # Extract the full date
         full_date = search_result.group(1)
-        # Extract just the year part from the full date
         year = re.search(r"\d{4}", full_date).group()
         return year
     else:
         return "Fiscal year not found in the document."
 
-
+"""
+Main function that does sentiment analysis on a sectino of MD&A section and stores results in a dict
+"""
 def analyze_mdna_and_stock(year, percent_change_stock, mdna_text, results_dict):
 
     prompt = f"You should only return a single number and nothing else. Analyze the following text from the MD&A section for the year {year}, and provide a numeric sentiment score from -100 to 100 reflecting the company's performance where 100 is the most optimal performance and -100 is the worst performance:\n\n{mdna_text}"
@@ -127,19 +125,19 @@ def analyze_mdna_and_stock(year, percent_change_stock, mdna_text, results_dict):
     )
 
     try:
-        # Attempt to extract a numeric sentiment score from the LLM output
         response_text = chat_completion.choices[0].message.content.strip()
         sentiment_score = int(re.search(r'(-?\d+)', response_text).group())
         sentiment_score = categorize_sentiment(sentiment_score)
     except (AttributeError, ValueError):
-        # If unable to find or convert to an integer, handle the error
         sentiment_score = None
         print("Failed to extract a numeric sentiment score from the LLM response.")
 
     results_dict[year] = (sentiment_score, percent_change_stock)
     return results_dict
 
-
+"""
+Helper method to standardize the sentiment score to account for LLM nuances to only be multiples of 25
+"""
 def categorize_sentiment(score):
     if score < -87.5:
         return -100
@@ -160,7 +158,9 @@ def categorize_sentiment(score):
     else:
         return 100
 
-
+"""
+Extracts the percent change of a stock over a year
+"""
 def get_yearly_stock_performance(ticker, year):
     # Construct the date strings for the start and end of the year
     start_date = f"{year}-01-01"
@@ -185,7 +185,9 @@ def get_yearly_stock_performance(ticker, year):
     else:
         return None
 
-
+"""
+This method runs a sentiment analysis on each of the 10-ks for a given ticker and stores the results in dict that is returned
+"""
 def process_files(root_dir, ticker):
     results_dict = {}
 
@@ -200,23 +202,19 @@ def process_files(root_dir, ticker):
                 # Process each full-submission.txt file
                 cleaned_text = process_file(full_submission_path)
 
-
-                # Extract the fiscal year from the document
                 fiscal_year = extract_fiscal_year(cleaned_text)
                 print(fiscal_year)
 
                 if fiscal_year.isdigit():
-                    # Get the stock performance for the given year
                     percent_change_stock = get_yearly_stock_performance(ticker, fiscal_year)
 
                     if percent_change_stock is not None:
-                        # Analyze the MD&A section and update results dictionary
                         mdna_section = extract_mdna(cleaned_text)
                         truncated_mdna_section = truncate_string_to_max_tokens(mdna_section, "gpt-3.5-turbo")
                         results_dict = analyze_mdna_and_stock(fiscal_year, percent_change_stock,
                                                               truncated_mdna_section, results_dict)
 
-        # After processing, delete the entire ticker_path folder
+        # Delete the entire ticker_path folder after done
         try:
             path = os.path.join(root_dir, ticker)
             shutil.rmtree(path)
@@ -230,60 +228,25 @@ def process_files(root_dir, ticker):
     results_dict = sort_results_by_year(results_dict)
     return results_dict
 
-
+"""
+Helper method to truncate an input to an LLM given a token limit to prevent errors
+"""
 def truncate_string_to_max_tokens(text, model="gpt-3.5-turbo", max_tokens=15000):
-    """
-    Truncate a single string to fit within the specified token limit for the given model.
-
-    Args:
-        text (str): Input text to be truncated.
-        model (str): Model name, e.g., "gpt-3.5-turbo" or "gpt-4-turbo".
-        max_tokens (int): Maximum token limit for the given model.
-
-    Returns:
-        str: Truncated text.
-    """
-    # Get encoding for the model
     encoding = tiktoken.encoding_for_model(model)
-
-    # Encode the text and count tokens
     tokens = encoding.encode(text)
 
-    # Truncate tokens if necessary
     truncated_tokens = tokens[:max_tokens]
-
-    # Decode back to text
     truncated_text = encoding.decode(truncated_tokens)
 
     return truncated_text
 
+"""
+Helper method to sort the dict returned by process_files by filing year
+"""
 def sort_results_by_year(results_dict):
-    """
-    Sorts the results dictionary by year.
-
-    Args:
-        results_dict (dict): The results dictionary to be sorted.
-
-    Returns:
-        dict: Sorted results dictionary by year.
-    """
-    # Convert to list of tuples and sort by year (integer conversion)
     sorted_tuples = sorted(results_dict.items(), key=lambda item: int(item[0]))
 
-    # Convert back to a regular dictionary
     return dict(sorted_tuples)
-
-
-# results = {'2008': (75, -56), '2006': (25, 17), '2019': (0, 88), '1999': (-50, 100), '2012': (0, 24), '2017': (50, 46), '2005': (50, 100), '2009': (50, 100), '2022': (75, -26), '2015': (75, -3), '2016': (-75, 12), '2007': (-25, 100), '2021': (50, 33), '2003': (-25, 48), '2002': (0, -36), '2018': (75, -8), '2020': (25, 80), '2011': (50, 24), '2023': (-25, 47), '1995': (0, -18), '2004': (50, 100), '2000': (0, -71), '2010': (75, 51), '1996': (0, -32), '1997': (0, -37), '2014': (25, 41), '2013': (75, 0)}
-# results = sort_results_by_year(results)
-
-
-# results = process_files("/Users/grantgaffney/PycharmProjects/TCKRAnalysis/sec-edgar-filings", "TSLA")
-# print(results)
-
-
-#{'1995': (0, -18), '1996': (0, -32), '1997': (-50, -37), '1999': (-50, 100), '2000': (-50, -71), '2002': (-50, -36), '2003': (-50, 48), '2004': (0, 100), '2005': (50, 100), '2006': (75, 17), '2007': (50, 100), '2008': (25, -56), '2009': (50, 100), '2010': (50, 51), '2011': (0, 24), '2012': (25, 24), '2013': (50, 0), '2014': (-25, 41), '2015': (75, -3), '2016': (-50, 12), '2017': (75, 46), '2018': (100, -8), '2019': (-50, 88), '2020': (25, 80), '2021': (75, 33), '2022': (75, -26), '2023': (-75, 47)}
-#TSLA {'2010': (-75, 39), '2011': (-50, 6), '2012': (-75, 14), '2013': (75, 100), '2014': (50, 48), '2015': (-50, 6), '2016': (0, -7), '2017': (100, 44), '2018': (0, 7), '2019': (50, 35), '2020': (75, 100), '2021': (50, 48), '2022': (75, -67)}
 
 
 # Load cached data or create an empty dictionary if not available
@@ -294,10 +257,15 @@ if os.path.exists(cache_file_path):
 else:
     cached_data = {}
 
-# Initialize the Flask app
+
+
 app = Flask(__name__)
 CORS(app)
 
+
+"""
+If ticker has not been analyzed analyze it, then display result
+"""
 def fetch_and_analyze_ticker(ticker):
     if ticker in cached_data and "results" in cached_data[ticker] and "analysis" in cached_data[ticker]:
         return cached_data[ticker]
@@ -326,17 +294,24 @@ def fetch_and_analyze_ticker(ticker):
 
     return cached_data[ticker]
 
-
+"""
+Helper method to calculate the average displacement from a dict
+"""
 def calculate_average_displacement(sentiment_scores, stock_changes):
     differences = np.abs(np.array(sentiment_scores) - np.array(stock_changes))
     return np.mean(differences)
-
+"""
+Helper method to calculate the coorelation of a dict
+"""
 def calculate_correlation(sentiment_scores, stock_changes):
     if len(sentiment_scores) == 0 or len(stock_changes) == 0:
         return None
     correlation, _ = pearsonr(sentiment_scores, stock_changes)
     return correlation
 
+"""
+Method to use OpenAI LLM API to get extra insight based on the sentiment scores and stock performances
+"""
 def generate_analysis(results, correlation, avg_displacement):
     years = list(results.keys())
     sentiment_scores = [result[0] for result in results.values()]
@@ -359,25 +334,21 @@ def generate_analysis(results, correlation, avg_displacement):
         ],
         model="gpt-3.5-turbo"
     )
+
     return chat_completion.choices[0].message.content.strip()
 
+"""
+Shift the dict so that the current year of a 10-k is matched with the next year's performance
+"""
 def shift_stock_performance(results):
-    """
-    Shifts the stock performance back by one year to align with the sentiment scores.
-    Excludes the last year as no next-year performance data is available.
-
-    Args:
-        results (dict): The results dictionary with years as keys.
-
-    Returns:
-        dict: Shifted results with sentiment scores aligned to the next year's stock changes.
-    """
     sorted_years = sorted(results.keys())
     shifted_results = {sorted_years[i]: results[sorted_years[i + 1]] for i in range(len(sorted_years) - 1)}
 
     return shifted_results
 
-
+"""
+Method for webapp to get data to populate the chart and analysis section
+"""
 @app.route('/api/data', methods=['POST'])
 def get_data():
     data = request.get_json()
@@ -408,6 +379,9 @@ def get_data():
         "analysis": analysis
     })
 
+"""
+Helper method for webapp to create the stock slider across the top of the screen
+"""
 @app.route('/api/stocks', methods=['GET'])
 def get_stocks():
     symbols = [
